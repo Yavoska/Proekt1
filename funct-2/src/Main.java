e3b6b33fb956970baa409901b44f197377167173
Yavoska <artem.yavorskiy.77@gmail.com>
2022-10-13 13:32:27 +0200

    added Function1

---
+++ funct-2/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Main {
+    public static int[] function1() {
+        int[] array = new int[6];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) Math.round((Math.random() * 40) - 10);
+            System.out.println(array[i]+",");
+
+        }
+        return array;
+    }
+    public static void main(String[] args) {
+        System.out.print("Choose Function" + "\n");
+        int[] result = function1();
+        System.out.print(Arrays.toString(result));
+        //int[] result2=function2();
+        //System.out.print();
+    }
+}
